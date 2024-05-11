5c07fe49d0ab54256e708deab02a08b2f0cfd7ea
ajr Handsome <1113635707@qq.com>
2024-05-11 13:46:50 +0800

    00

---
+++ src/main/java/com/example/ajrplatform/AjrPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.ajrplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjrPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjrPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ajrplatform/AjrPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ajrplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjrPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
